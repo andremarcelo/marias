7f5e473bcbe0da6e98c39284ff3681772f167c3c
jenyashvartsman <jenyashvartsman@gmail.com>
2018-02-16 10:45:59 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './common/app-header/app-header.component';
+import { DashboardViewComponent } from './view/dashboard-view/dashboard-view.component';
 
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {path: 'dashboard', redirectTo: 'DashboardViewComponent'},
+  {path: '**', component: DashboardViewComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    DashboardViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
